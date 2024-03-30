3ea7f63430fbef1ceed5d3312b27de3c189ad781
Eris <erisfiregamer1@gmail.com>
2024-03-30 19:33:00 -0400

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,28 @@
+// This module is available on JSR as @erisws/importLLMFile
+
+// Automatically imports an LLM File to the global object (availableLLMs) and gives you information about it so you don't have to waste time writing an implementation.
+
+import * as types from "./llmFile.d.ts"
+
+export default async function importLLMFile(modulePath: string) {
+  try {
+    if (!globalThis.availableLLMs) {
+      globalThis.availableLLMs = {}
+    }
+
+    const module: types.llmFile = await import(`${modulePath}`);
+
+    if (module && module.information && typeof module.send === "function") {
+        globalThis.availableLLMs[module.information.id] = {
+        information: module.information,
+        send: module.send,
+      };
+      return module.information; // Return the information object
+    } else {
+      return null; // Return null if the module doesn't have the required exports
+    }
+  } catch (error) {
+    console.error(`Error importing module ${modulePath}':`, error);
+    return null; // Return null if there's an error importing the module
+  }
+}
